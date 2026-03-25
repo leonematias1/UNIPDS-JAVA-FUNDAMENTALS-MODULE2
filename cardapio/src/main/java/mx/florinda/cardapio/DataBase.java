c34ec200586a1767b170cdbbee23a0e5239230a1
Leone <leonematias9@gmail.com>
2026-03-25 17:59:00 -0300

    25-03-2026

--- cardapio/src/main/java/mx/florinda/cardapio/DataBase.java
+++ cardapio/src/main/java/mx/florinda/cardapio/DataBase.java
@@ -63,54 +63,8 @@ public class DataBase {
     }
 
     public List<ItemCardapio> listaDeItemCardapio(){
-
-        List<ItemCardapio> items = new ArrayList<>();
-        ItemCardapio refrescoDoChaves = new ItemCardapio(1L, "Refresco do Chaves", """
-                Suco de limão, que parece tamarindo mas tem gosto de groselha.
-                """, BEBIDAS, new BigDecimal("2.99"), null);
-        items.add(refrescoDoChaves);
-
-        ItemCardapio sanduicheDoChaves = new ItemCardapio(2L, "Sanduíche de presunto do Chaves", """
-                Sanduíche de presunto simples, mas feito com muito amor.
-                """, PRATOS_PRINCIPAIS, new BigDecimal("3.50"), new BigDecimal("2.99"));
-        items.add(sanduicheDoChaves);
-
-        ItemCardapio tortaDaDonaFlorinda = new ItemCardapio(5L, "Torta da Dona Florinda", """
-                Torta de frango com recheio cremoso e massa crocante.
-                """, PRATOS_PRINCIPAIS, new BigDecimal("12.99"), new BigDecimal("10.00"));
-        items.add(tortaDaDonaFlorinda);
-
-        ItemCardapio pipocaDoQuico = new ItemCardapio(6L, "Pipoca do Quico", """
-                Balde de pipoca preparado com carinho pelo Quico.
-                """, PRATOS_PRINCIPAIS, new BigDecimal("4.99"), new BigDecimal("3.99"));
-        items.add(pipocaDoQuico);
-
-        ItemCardapio aguaDeJamaica = new ItemCardapio(7L, "Água de jamaica", """
-                Água aromatizada com hibisco e toque de açúcar.
-                """, BEBIDAS, new BigDecimal("2.50"), new BigDecimal("2.00"));
-        items.add(aguaDeJamaica);
-
-        ItemCardapio cafeDaDonaFlorinda = new ItemCardapio(8L, "Café da Dona Florinda", """
-                Café forte para começar o dia com energia.
-                """, BEBIDAS, new BigDecimal("1.99"), new BigDecimal("1.50"));
-        items.add(cafeDaDonaFlorinda);
-
-        ItemCardapio churrosDoChaves = new ItemCardapio(9L, "Churros do Chaves", """
-                Churros recheados com doce de leite, clássico e irresistível.
-                """, SOBREMESA, new BigDecimal("4.99"), new BigDecimal("3.99"));
-        items.add(churrosDoChaves);
-
-        ItemCardapio GelatinaDoNhonho = new ItemCardapio(10L, "Gelatina do Nhonho", """
-                Gelatina de várias cores, a sobremesa favorita do Nhonho.
-                """, SOBREMESA, new BigDecimal("3.50"), new BigDecimal("2.99"));
-        items.add(GelatinaDoNhonho);
-
-        ItemCardapio boloDaDonaClotilde = new ItemCardapio(11L, "Bolo de Chocolate da Dona Clotilde", """
-                Bolo de Chocolate com cobertura de brigadeiro.
-                """, SOBREMESA, new BigDecimal("5.99"), new BigDecimal("4.99"));
-        items.add(boloDaDonaClotilde);
-
-        return items;
+        List<ItemCardapio> listItens = itensPorId.values().stream().toList();
+        return listItens;
     }
 
     public ItemCardapio findById(Long id){
@@ -126,6 +80,7 @@ public class DataBase {
     public void listItemCardapioMap(){
         itensPorId.forEach((aLong, itemCardapio) -> System.out.println(itemCardapio));
     }
+
     public boolean removerItemCardapio(Long idParaRemover) {
         ItemCardapio itemCardapioRemovido = itensPorId.remove(idParaRemover);
         return itemCardapioRemovido != null;
@@ -148,6 +103,15 @@ public class DataBase {
                 (itemAntigo, novoPreco) ->
                         System.out.printf("\n - %s: %s => %s\n", itemAntigo.nome(), itemAntigo.preco(), novoPreco));
         System.out.println();
+    }
+
+    public void addItemCardapio(ItemCardapio itemCardapio){
+        itensPorId.put(itemCardapio.id(), itemCardapio);
+    }
+
+    public DataBase(){
+        insertDataAtMap();
+        listaDeItemCardapio();
     }
 
 }
--- cardapio/src/main/java/mx/florinda/cardapio/ServidorItensCardapioSocket.java
+++ cardapio/src/main/java/mx/florinda/cardapio/ServidorItensCardapioSocket.java
@@ -1,5 +1,7 @@
 package mx.florinda.cardapio;
 
+import com.google.gson.Gson;
+
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
@@ -7,43 +9,102 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServidorItensCardapioSocket {
 
+    private static final DataBase database = new DataBase();
+
     static void main(String[] args) throws Exception{
 
-        try(ServerSocket serverSocket = new ServerSocket(8000)){
-            System.out.println("Subiu o Servidor");
-
-            while (true){
-                try(Socket clientSocket = serverSocket.accept()){
-                    InputStream clientIS = clientSocket.getInputStream();
-
-                    StringBuilder requestBuilder = new StringBuilder();
-                    int data;
-                    do {
-                        data = clientIS.read();
-                        requestBuilder.append((char) data);
-                    }while (clientIS.available() >0);
-                    String request = requestBuilder.toString();
-                    System.out.println(request);
-
-                    Path path = Path.of("itensCardapio.json");
-                    String json = Files.readString(path);
-
-                    OutputStream clientOS = clientSocket.getOutputStream();
-                    PrintStream clientOut = new PrintStream(clientOS);
-
-                    clientOut.println("HTTP/1.1 200 OK");
-                    clientOut.println("Content-type: application/json; charset=UTF-8");
-                    clientOut.println();
-                    clientOut.println(json);
-
+        try(ExecutorService executorService = Executors.newFixedThreadPool(50)){
+            try(ServerSocket serverSocket = new ServerSocket(8000)){
+                System.out.println("Subiu o Servidor");
+                while (true){
+                    Socket clientSocket = serverSocket.accept();
+                    executorService.execute(() -> trataRequisicao(clientSocket));
                 }
             }
-
-
         }
+
+    }
+
+    private static void trataRequisicao(Socket clientSocket) {
+        try (clientSocket){
+            InputStream clientIS = clientSocket.getInputStream();
+
+            StringBuilder requestBuilder = new StringBuilder();
+            int data;
+            do {
+                data = clientIS.read();
+                requestBuilder.append((char) data);
+            }while (clientIS.available() >0);
+
+            String request = requestBuilder.toString();
+            System.out.println(request);
+
+            System.out.println("---------------------------------");
+            System.out.println("\n\n\n Chegou um novoRequest");
+            Thread.sleep(250);
+
+            String[] requestChunks = request.split("\r\n\r\n");
+            String requestLineAndHeader = requestChunks[0];
+            String[] requestLineAndHeaderChunks = requestLineAndHeader.split("\r\n");
+
+            String methodAndURI = requestLineAndHeaderChunks[0];
+
+            System.out.println("---------------------------------");
+            OutputStream clientOS = clientSocket.getOutputStream();
+            PrintStream clientOut = new PrintStream(clientOS);
+
+            if(methodAndURI.contains("GET /itensCardapio.json")){
+                System.out.println("Chamou itensCardapio.json");
+                Path path = Path.of("itensCardapio.json");
+                String json = Files.readString(path);
+                clientOut.println("HTTP/1.1 200 OK");
+                clientOut.println("Content-type: application/json; charset=UTF-8");
+                clientOut.println();
+                clientOut.println(json);
+            }else if(methodAndURI.contains("GET /itens-cardapio")){
+                System.out.println("Chamou itens-cardapio");
+                List<ItemCardapio> listItens = database.listaDeItemCardapio();
+                Gson gson = new Gson();
+                String json = gson.toJson(listItens);
+                clientOut.println("HTTP/1.1 200 OK");
+                clientOut.println("Content-type: application/json; charset=UTF-8");
+                clientOut.println();
+                clientOut.println(json);
+            }else if(methodAndURI.contains("GET /total-itens-cardapio")){
+                System.out.println("Chamou itens-cardapio/total");
+                List<ItemCardapio> listItens = database.listaDeItemCardapio();
+                clientOut.println("HTTP/1.1 200 OK");
+                clientOut.println("Content-type: application/json; charset=UTF-8");
+                clientOut.println();
+                clientOut.println(listItens.size());
+            } else if(methodAndURI.contains("POST /add-itens-cardapio")){
+                System.out.println("Chamou itens-cardapio/total");
+
+                if (requestChunks.length == 1) {
+                    clientOut.println("HTTP/1.1 400 Bad Request");
+                    return;
+                }else{
+                    String body = requestChunks[1];
+                    Gson gson = new Gson();
+                    ItemCardapio itemCardapio = gson.fromJson(body, ItemCardapio.class);
+                    database.addItemCardapio(itemCardapio);
+                }
+
+                clientOut.println("HTTP/1.1 201 CREATED");
+            } else {
+                System.out.println("URI Não encontrada "+methodAndURI);
+                clientOut.println("HTTP/1.1 404 Not Found");
+            }
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        //curl -v -X POST -d '{"id":240,"nome":"ITEM 240","descricao":"ITEM 240.\n","categoria":"BEBIDAS","preco":2.99}' -H 'Content-Type: application/json' http://localhost:8000/add-itens-cardapio
 
     }
 
